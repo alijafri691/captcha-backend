14554d6d794867381b11fd80efe53809542324f5
alijafri691 <alijafri691@gmail.com>
2025-04-23 06:06:55 +0530

    Update solve.js logic@

--- api/solve.js
+++ api/solve.js
@@ -5,9 +5,10 @@ const API_KEY = process.env.TWOCAPTCHA_API_KEY;
 
 module.exports = async (req, res) => {
   // Always set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end(); // Respond to preflight
